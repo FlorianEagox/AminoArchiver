580c36ba8fb844d89e839c9bcd3cd08d01c5d366
Seth Painter <timytomy11@gmail.com>
2020-05-15 02:10:47 -0500

    Added ability to read page and find all text in currently loaded messages

--- content.js
+++ content.js
@@ -1,1 +1,28 @@
-alert("Hello there");
+let parentSelector = '.message-list > *';
+let messageClass = 'chat-message';
+let sentFromMe = 'from-me';
+class Message {
+	constructor(userID, messageText, timestampID) {
+		this.userID = userID;
+		this.messageText = messageText;
+		this.timestampID = timestampID;
+	}
+	static generateFromHTML(item) {
+		// Gets the element containing the actual message content
+		console.log(item)
+		let messageText = item.children[1].children[item.classList.contains(sentFromMe) ? 0 : 1].innerText;
+
+		return new Message(null, messageText, null);
+	}
+}
+async function parseMessages() {
+	let chatItems = document.querySelectorAll(parentSelector);
+	let messages = [];
+	await (chatItems.forEach(item => {
+		if (item.classList.contains(messageClass))
+			messages.push(Message.generateFromHTML(item));
+	}));
+
+	messages.forEach(message => console.log(message.messageText));
+}
+parseMessages();
