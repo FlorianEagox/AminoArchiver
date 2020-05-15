72ff9632321ce0db2fa21628f989f2e0c9e83f46
Seth Painter <timytomy11@gmail.com>
2020-05-15 02:57:47 -0500

    Changed message access class to exclude info messages. Added saving most recent timestamp.

--- content.js
+++ content.js
@@ -1,28 +1,37 @@
+console.clear();
 let parentSelector = '.message-list > *';
-let messageClass = 'chat-message';
+let messageClass = 'user-message';
 let sentFromMe = 'from-me';
+let timeStampClass = 'timestamp';
 class Message {
 	constructor(userID, messageText, timestampID) {
 		this.userID = userID;
 		this.messageText = messageText;
 		this.timestampID = timestampID;
 	}
-	static generateFromHTML(item) {
-		// Gets the element containing the actual message content
-		console.log(item)
+	static generateFromHTML(item, currentTimestamp) {
+		// Get the text of messages, if the message is sent from the user, get the 0th child, otherwise, get the first child.
 		let messageText = item.children[1].children[item.classList.contains(sentFromMe) ? 0 : 1].innerText;
-
-		return new Message(null, messageText, null);
+		return new Message(null, messageText, currentTimestamp);
 	}
 }
-async function parseMessages() {
+function parseMessages() {
 	let chatItems = document.querySelectorAll(parentSelector);
 	let messages = [];
-	await (chatItems.forEach(item => {
-		if (item.classList.contains(messageClass))
-			messages.push(Message.generateFromHTML(item));
-	}));
+	let timestamps = [];
+	currentTimestamp = -1;
+	chatItems.forEach(item => {
+		if (item.classList.contains(messageClass)) {
+			// TODO change the way the class is generated
+			let timestamp = timestamps[currentTimestamp];
+			messages.push(Message.generateFromHTML(item, timestamp != null ? currentTimestamp : null));
+		} else if (item.classList.contains(timeStampClass)) {
+			timestamps.push(item.innerText);
+			currentTimestamp++;
+		}
+	});
 
-	messages.forEach(message => console.log(message.messageText));
+	messages.forEach(message =>
+		console.log(timestamps[message.timestampID], message.messageText));
 }
 parseMessages();
